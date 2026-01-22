d27bbb65377a7e2380c918ca4dc9ec334153f906
HarryColyer <harrycolyer@hotmail.co.uk>
2026-01-22 23:06:07 +0000

    added: styling

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -3,6 +3,7 @@ import ContractForm from "../components/WorkOrderForm";
 import SignUpForm from "../components/SignUpForm";
 import ContactContainer from "../components/ContractList";
 import logo from "../assets/pestflowlogo.png";
+import icon from "../assets/pesticon.png";
 import TechnicianList from "../components/TechnicianList";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
@@ -15,21 +16,28 @@ const AdminDashboard = () => {
   const [isTechFormOpen, setIsTechFormOpen] = useState(false);
   const [isContactContainerOpen, setIsContactContainerOpen] = useState(false);
   const [isTechContainerOpen, setIsTechContainerOpen] = useState(false);
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
 
   return (
-    <div className="custom-bg-color min-h-screen text-white flex flex-col px-12">
-      <nav className="flex justify-between items-center py-4">
-        <img className="size-32" src={logo} alt="Logo" />
+    <div className="custom-bg-color min-h-screen text-white flex flex-col px-4">
+      <nav className="flex justify-between">
+        <img className="size-32 hidden md:block" src={logo} alt="Logo" />
+        <img className="size-16 md:hidden" src={icon} alt="Logo" />
         {/* Buttons to open forms */}
-        <div>
+        <div className="flex gap-4 items-center">
           <button onClick={() => setIsContractFormOpen(!isContractFormOpen)}>
             <FontAwesomeIcon icon={faFileCirclePlus} />
           </button>
           <button onClick={() => setIsTechFormOpen(!isTechFormOpen)}>
             <FontAwesomeIcon icon={faUserPlus} />
           </button>
-          <button
+          <button className="border-2 border-white h-8 px-3 rounded" onClick={logout}>Logout</button>
+        </div>
+      </nav>
+      <h1 className="text-3xl my-6">
+        Welcome {user?.name && user.name[0].toUpperCase() + user.name.slice(1)}
+      </h1>
+      <button
             onClick={() => setIsContactContainerOpen(!isContactContainerOpen)}
           >
             Contracts
@@ -37,12 +45,6 @@ const AdminDashboard = () => {
           <button onClick={() => setIsTechContainerOpen(!isTechContainerOpen)}>
             Technicians
           </button>
-        </div>
-      </nav>
-      <h1>
-        Welcome {user?.name && user.name[0].toUpperCase() + user.name.slice(1)}
-      </h1>
-
       {isContractFormOpen && (
         <div>
           <ContractForm />
